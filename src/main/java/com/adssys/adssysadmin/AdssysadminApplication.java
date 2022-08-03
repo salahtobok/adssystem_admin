1d939b155234467d74a8062657868bdef39f412a
EADN_PC <salahtobok1@gmail.com>
2022-08-03 19:38:41 +0100

    first commit

---
+++ src/main/java/com/adssys/adssysadmin/AdssysadminApplication.java
@@ -0,0 +1,13 @@
+package com.adssys.adssysadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdssysadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdssysadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adssys/adssysadmin/AdssysadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adssys.adssysadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdssysadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
